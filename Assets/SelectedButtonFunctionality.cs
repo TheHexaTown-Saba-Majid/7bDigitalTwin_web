397456ddf868e0a0a1fc67d4188b34744a80a4b1
TheHexaTown-Saba-Majid <saba.majid@thehexatown.com>
2024-05-31 15:03:18 +0500

    Selected Button functionality

--- Assets/SelectedButtonFunctionality.cs
+++ Assets/SelectedButtonFunctionality.cs
@@ -3,51 +3,26 @@ using UnityEngine.UI;
 
 public class SelectedButtonFunctionality : MonoBehaviour
 {
-    public GameObject[] parentGameObjects; // Array of parent GameObjects
-    public GameObject[] childGameObjects; // Array of child GameObjects
-    private Image[] parentImages; // Array of parent Image components
-    private Image[] childImages; // Array of child Image components
+    public Button[] buttons;
     private void Start()
     {
-        // Initialize the arrays of parent and child image components
-        parentImages = new Image[parentGameObjects.Length];
-        childImages = new Image[childGameObjects.Length];
-        // Get references to the image components
-        for (int i = 0; i < parentGameObjects.Length; i++)
+        foreach (Button button in buttons)
         {
-            parentImages[i] = parentGameObjects[i].GetComponent<Image>();
-        }
-        for (int i = 0; i < childGameObjects.Length; i++)
-        {
-            childImages[i] = childGameObjects[i].GetComponent<Image>();
+            button.onClick.AddListener(() => OnButtonClicked(button));
         }
     }
-    public void OnButtonClick()
+    private void OnButtonClicked(Button clickedButton)
     {
-        // Toggle the visibility of the parent's image components
-        foreach (var parentImage in parentImages)
+        foreach (Button button in buttons)
         {
-            parentImage.enabled = !parentImage.enabled;
-        }
-        // Toggle the visibility of each child's image components
-        foreach (var childImage in childImages)
-        {
-            childImage.enabled = !childImage.enabled;
-        }
-        // Turn off all other child images in sibling buttons
-        var siblingButtons = transform.parent.GetComponentsInChildren<SelectedButtonFunctionality>();
-        foreach (var siblingButton in siblingButtons)
-        {
-            if (siblingButton != this)
+            Image childImage = button.GetComponentInChildren<Image>();
+            if (button == clickedButton)
             {
-                foreach (var siblingChildImage in siblingButton.childImages)
-                {
-                    siblingChildImage.enabled = false;
-                }
-                foreach (var siblingParentImage in siblingButton.parentImages)
-                {
-                    siblingParentImage.enabled = true;
-                }
+                childImage.enabled = true;
+            }
+            else
+            {
+                childImage.enabled = false;
             }
         }
     }
